79069b78d638d1b7847e5ded4caa6dffa2559463
Jason Hobbs <jason.hobbs@calxeda.com>
2012-05-29 10:57:15 -0500

    drop the 'common' layer
    it was the only subdir in cx_manage_util, so just move its contents up.

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@ from setuptools import setup
 
 setup(name='cx_manage_util',
     version='0.0.1',
-    packages=['cx_manage_util',
-              'cx_manage_util.common',
-              'cx_manage_util.cxfwupd'],
+    packages=['cx_manage_util'],
     scripts=['scripts/cxmanage']
 )
