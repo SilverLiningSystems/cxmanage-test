acffebc4b2fbec1e0e703efb72fdcca2a810c8e5
George Kraft <george.kraft@calxeda.com>
2012-06-04 14:43:03 -0500

    setup.py: add "pexpect" and "tftpy" as dependencies
    Also updated the README with info about installing in a virtualenv.

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(name='cx_manage_util',
     version='0.0.1',
     packages=['cx_manage_util'],
-    scripts=['scripts/cxmanage']
+    scripts=['scripts/cxmanage'],
+    install_requires=['pexpect', 'tftpy']
 )
