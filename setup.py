9b06a903ab5b01957ed15e1bbd576e649607d39a
George Kraft <george.kraft@calxeda.com>
2012-06-15 12:33:48 -0500

    Add 'cxpackage' script
    This provides a more convenient way to create firmware packages.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='cxmanage',
     version='0.2.0',
     packages=['cxmanage'],
-    scripts=['scripts/cxmanage'],
+    scripts=['scripts/cxmanage', 'scripts/cxpackage'],
     description='Calxeda Management Utility',
     install_requires=['pexpect', 'tftpy', 'pyipmi'],
     classifiers=[
