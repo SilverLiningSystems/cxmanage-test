f07a9349fe30cf58e051ef29c3704adfa805e111
George Kraft <george.kraft@calxeda.com>
2012-09-20 13:31:52 -0500

    Bump pyipmi requirement to 0.5.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     description='Calxeda Management Utility',
     # NOTE: As of right now, the pyipmi version requirement needs to be updated
     # in the scripts/cxmanage check_versions() function as well.
-    install_requires=['tftpy', 'pyipmi>=0.4.0', 'argparse'],
+    install_requires=['tftpy', 'pyipmi>=0.5.0', 'argparse'],
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7']
