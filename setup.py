5cd705b7c8fd23adf0ba100642c32457e27e188b
George Kraft <george.kraft@calxeda.com>
2012-12-12 15:04:33 -0600

    Bump pyipmi requirement to v0.6.2

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     # at the top of scripts/cxmanage as well.
     install_requires=[
                         'tftpy',
-                        'pyipmi>=0.6.1',
+                        'pyipmi>=0.6.2',
                         'argparse',
                         'sphinx',
                         'cloud_sptheme'
