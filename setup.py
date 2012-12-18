cd1ab668ec55b84e2e654e910c6376c4ec77b25f
George Kraft <george.kraft@calxeda.com>
2012-12-18 11:29:05 -0600

    Bump to v0.7.1

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ from setuptools import setup
 
 setup(
     name='cxmanage',
-    version='0.7.0',
+    version='0.7.1',
     packages=['cxmanage', 'cxmanage.commands', 'cxmanage_api'],
     scripts=['scripts/cxmanage'],
     package_data={'cxmanage_api': ['data/cids', 'data/registers']},
