db1c8faa73847b525f9a27dbb7310bb1b27bc2b4
George Kraft <george.kraft@calxeda.com>
2013-12-19 09:19:45 -0600

    nojira: Bump to v0.11.0, bump pyipmi/ipmitool requirements

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     install_requires=[
                         'tftpy',
                         'pexpect',
-                        'pyipmi>=0.9.1',
+                        'pyipmi>=0.11.0',
                         'argparse',
                         'unittest-xml-reporting',
                         'mock'
